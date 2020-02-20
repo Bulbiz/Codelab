532e65f96c5de5e1ced404f0d616b35a77fb2c85
Gregoire Marin <gregoire.marin@univ.etu-paris-diderot.fr>
2020-02-20 18:52:36 +0100

    ajout d'une classe englobant tous les éléments de la partie graphique du langage

---
+++ src/view/langage/LanguagePanel.java
@@ -0,0 +1,15 @@
+
+package src.view.langage;
+
+import java.util.*;
+import javax.swing.JPanel;
+
+class LanguagePanel extends JPanel {
+    
+    EditPanel editPanel;
+    ResourcePanel resourcePanel;
+
+    public LanguagePanel() {
+
+    }
+}
