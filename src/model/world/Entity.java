a65e1cd94c5f288874a6ca65d4e913d5f2eb049c
Pholasa <pholasa2@outlook.fr>
2020-02-20 10:21:17 +0100

    Create the Constructor for Entity

--- src/model/world/Entity.java
+++ src/model/world/Entity.java
@@ -4,14 +4,17 @@ package src.model.world;
 import java.util.*;
 
 /**
- * 
+ * An Entity is an object in the board of a level
  */
 public class Entity {
-
-    /**
-     * Default constructor
-     */
-    public Entity() {
+	protected Board levelBoard; 
+	protected int x;
+	protected int y;
+	
+    public Entity(Board b, int xStart, int yStart) {
+    	levelBoard = b;
+    	x = xStart;
+    	y = yStart;
     }
 
 
