8faede76103c2029384f3575cf12681deee52aa9
Pholasa <pholasa2@outlook.fr>
2020-02-24 20:08:14 +0100

    Mise en commentaire du board car eventuellement pas utile

--- src/view/world/BoardPanel.java
+++ src/view/world/BoardPanel.java
@@ -9,8 +9,9 @@ import java.util.*;
  * 
  */
 public class BoardPanel extends JPanel implements IDisplayable {
-	 
+	/*FIXME : Is the board really useful here since we have the cell already ?
 	private Board board;
+	*/
 	private CellPanel[][] cellPanels;
 	 
     /**
