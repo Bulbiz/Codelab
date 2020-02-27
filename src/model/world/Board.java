5b0a6447021c05272e691cc6d1558dd8cd716fff
Antoine KHOW <anto.kh@hotmail.fr>
2020-02-27 12:11:54 +0100

    Added label TODO

--- src/model/world/Board.java
+++ src/model/world/Board.java
@@ -23,7 +23,7 @@ public class Board {
     
     //This method will create a border on the board that won't be crossable
     private void createBorder() {
-    	Cell mountain = new Cell(); //La case qui servira de Bordure à déterminer
+    	Cell mountain = new Cell(); //TODO La case qui servira de Bordure à déterminer
     	for(int i = 0; i < this.cells[0].length; i++) {
     		this.cells[0][i] = new Cell();
     		this.cells[16][i] = new Cell();
