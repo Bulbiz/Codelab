9a0800a5269d07764086f817846a0b2fd459b7d9
Thierry <chhoathierryprojet@gmail.com>
2020-03-04 23:38:48 +0100

    implémentation du contructeur des sous classe de Decor

--- src/model/world/Wall.java
+++ src/model/world/Wall.java
@@ -4,14 +4,15 @@ package src.model.world;
 import java.util.*;
 
 /**
- * 
+ *
  */
 public class Wall extends Decor {
 
     /**
      * Default constructor
      */
-    public Wall() {
+    public Wall(Board b, int xPosition, int yPosition) {
+        super(b,xPosition,yPosition);
     }
 
 }
