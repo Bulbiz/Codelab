cd245df0e642686c228c5906a319376bf6b4c464
Pholasa <pholasa2@outlook.fr>
2020-03-06 10:26:17 +0100

    Remise A zéro de la vue du board

--- src/view/world/LevelPanel.java
+++ src/view/world/LevelPanel.java
@@ -14,7 +14,6 @@ public class LevelPanel extends JPanel{
 	
 	private ControllerWorld worldController;
 	
-	private BoardPanel worldView;
 	private JButton runButton;
 	
 	private JButton stopButton;
@@ -23,7 +22,6 @@ public class LevelPanel extends JPanel{
 		this.level = l;
 		this.worldController = new ControllerWorld (this.level, this);
 		
-		this.worldView = new BoardPanel(this.level.getBoard());
 		this.runButton = new JButton ("Run");
 		this.stopButton = new JButton ("Stop");
 		this.stopButton.addActionListener((e) -> worldController.stop());
@@ -37,17 +35,14 @@ public class LevelPanel extends JPanel{
 	//FIXME : Magic Number is not the best
 	private void layoutPlacement() {
 		this.setLayout(null);
-		this.add(this.worldView);
 		this.add(this.runButton);
 		this.add(this.stopButton);
-		this.worldView.setBounds(0,0,500,500);
 		this.runButton.setBounds(400, 510, 100, 75);
 		this.stopButton.setBounds(300, 510 , 100, 75);
 	}
 	
 	//FIXME There is no animation it's Sad :(
 	public void updateDisplay() {
-		this.worldView.updateDisplay();
 	}
 	
 	public void setEnableRunButton(boolean activation) {
