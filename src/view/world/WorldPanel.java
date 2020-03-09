654d7747a1d06598e2d7538f96167c30da5c770c
Pholasa <pholasa2@outlook.fr>
2020-03-09 13:41:27 +0100

    Save

--- src/view/world/WorldPanel.java
+++ src/view/world/WorldPanel.java
@@ -32,17 +32,18 @@ public class WorldPanel extends JPanel implements IDisplayable {
     
     public void paintComponent(Graphics g) {
     	super.paintComponent(g);
+    	System.out.println("Je suis appeler");
     	ImageIcon img = new ImageIcon(this.getClass().getResource("image/Sol.png"));
         //Draw Here;
     	for(int i=0; i< 17 ; i++)
     		for(int j = 0 ; j < 17 ; j ++) {
     			g.drawImage(img.getImage(),i*tileLength, j*tileLength,null);
     		}
-    	ImageIcon img2 = new ImageIcon(this.getClass().getResource("image/Transparence.png"));
+    	ImageIcon img2 = new ImageIcon(this.getClass().getResource("image/Front.gif"));
     	img2.paintIcon(this, g, 2*tileLength, 5*tileLength);
-    } 
+    }
+    
     public void updateDisplay() {
-    	repaint();
     }
 
 }
