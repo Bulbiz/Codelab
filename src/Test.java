00b4bae5dfa6fd3f50b70763603b474615b21873
Thierry <chhoathierryprojet@gmail.com>
2020-03-13 09:39:38 +0100

    test

--- src/Test.java
+++ src/Test.java
@@ -8,12 +8,15 @@ import src.view.*;
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.FileReader;
+import org.json.*;
 
 
 class Test {
     //test of running the project
     public static void begin(){
-      ArrayList<Personage> p = new ArrayList();
+     /* ArrayList<Personage> p = new ArrayList();
       Board b = new Board(6, 1, p);
       Player steve = new Player(b, 6, 15, 1);
       Queue<src.model.langage.Action> action = new LinkedList <src.model.langage.Action>();
@@ -40,14 +43,16 @@ class Test {
       LevelPanel vueLevel = new LevelPanel (levelTest);
       JFrame testWindows = TestWorldView.createWindows ("Test");
       testWindows.setContentPane(vueLevel);
-      testWindows.pack();
+      testWindows.pack();*/
     }
 
-    public static void main(String[] args) {
-        TestLanguageModel.run();
+    public static void main(String[] args) throws Exception {
+        /*TestLanguageModel.run();
         TestWorldModel.run();
         TestLanguageView.run();
-        TestWorldView.run();
+        TestWorldView.run();*/
+	       
+
 
        // begin();
         System.out.println("fin de test");
