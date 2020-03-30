78f35dfbf2eb9997ad59f3ab2c664614d78c862b
Gregoire Marin <gregoire.marin@univ.etu-paris-diderot.fr>
2020-03-30 21:36:31 +0200

    voilà la base de l'éditeur de niveau

---
+++ src/controller/ControllerEditor.java
@@ -0,0 +1,11 @@
+
+package src.controller;
+
+import src.editor.view.CellElementGeneratorPanel;
+import src.model.world.*;
+
+public class ControllerEditor {
+
+    Cell dest;
+    CellElementGeneratorPanel source;
+}
