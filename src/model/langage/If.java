bcea09c2e9e4255e32cd6558797e7194f7e61fec
Thierry <chhoathierryprojet@gmail.com>
2020-04-02 20:51:17 +0200

    reglage d'un petit problème de null pointeur exception

--- src/model/langage/If.java
+++ src/model/langage/If.java
@@ -29,22 +29,23 @@ public class If extends ControlFlowStatement {
 
 	public int run() {
 
+        if(actions.peek() == null)       // end actions list for the if
+            return 1;
+
 		int verification = actions.peek().run();
 		while(verification == 0) {
 			actions.poll();
 			verification = actions.peek().run();
 		}
 
-		if(verification == 1){
-			actions.poll();
-			return 2;                          // continu to execute the actions list
-		}
-
         if(verification == 2){			//if is a controle flow statement, don't depile
 			return 2;
 		}
 
-        return 1;                              // end actions list for the if
+		if(verification == 1){
+			actions.poll();
+			return 2;                          // continu to execute the actions list
+		}
+        return 1;
 	}
-
 }
