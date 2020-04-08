0816ff0434a18a917d81b2fccf958d2e22a0a2c0
Pholasa Remy <pholasa2@outlook.fr>
2020-04-08 11:04:40 +0200

    Create Button + Name

--- src/editor/view/GeneratorsPanel.java
+++ src/editor/view/GeneratorsPanel.java
@@ -2,6 +2,8 @@
 package src.editor.view;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
+
 import src.controller.ControllerEditor;
 import java.awt.*;
 import java.awt.Dimension;
@@ -11,23 +13,41 @@ public class GeneratorsPanel extends JPanel {
     private DecorGeneratorPanel decor;
     private CollectableGeneratorPanel collectable;
     private PersonageGeneratorPanel personage;
+    private JTextField name;
+    private JButton create;
     
     public GeneratorsPanel(ControllerEditor c) {
         controller = c;
         this.decor = new DecorGeneratorPanel(c);
         this.collectable = new CollectableGeneratorPanel(c);
         this.personage = new PersonageGeneratorPanel(c);
+        this.name = fieldName();
+        this.create = buttonCreate();
         layoutPlacement();
     }
+    
     private void layoutPlacement() {
     	this.setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
     	this.addButton();
+    }
+    private JTextField fieldName() {
+    	JTextField text = new JTextField ("LevelName",20);
+    	text.setMaximumSize(text.getPreferredSize());
+    	return text;
+    }
+    
+    private JButton buttonCreate() {
+    	JButton c = new JButton ("Create");
+    	c.addActionListener((e)-> controller.create(name.getName()));
+    	return c;
     }
     
     private void addButton() {
     	this.addWithSeparation(decor);
     	this.addWithSeparation(collectable);
     	this.addWithSeparation(personage);
+    	this.addWithSeparation(name);
+    	this.addWithSeparation(create);
     }
     
     private void addWithSeparation(JComponent c) {
