6ffd8d1f715d44f5c6bbea864c2cfb22f887f504
Thierry <chhoathierryprojet@gmail.com>
2020-04-09 22:25:31 +0200

    creation d'une classe pour la page d'accueil(MenuPanel) du jeu + ajout de son actionlistener

---
+++ src/view/world/MenuPanel.java
@@ -0,0 +1,35 @@
+package src.view.world;
+import src.*;
+import src.model.langage.*;
+import src.model.world.*;
+import src.view.langage.*;
+import src.view.world.*;
+import src.view.*;
+import src.controller.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+public class MenuPanel extends JPanel{
+
+    public MenuPanel(JFrame parent){
+        GridLayout g = new GridLayout(1,2);
+        g.setHgap(200);
+        this.setLayout(g);
+
+        JButton load = new JButton("Load");
+        JButton editor = new JButton("Editor");
+
+        load.addActionListener((e) -> {
+            Test.loadLevelForMenu();
+            parent.dispose();
+        });
+
+        editor.addActionListener((e) -> {
+            //waiting for the implementation for the editor of lvl.
+        });
+
+        this.add(load);
+        this.add(editor);
+    }
+}
