122a5fd9163c7489ee6cddcc35acd558b9216fbc
Pholasa Remy <pholasa2@outlook.fr>
2020-04-28 20:38:30 +0200

    Remise a zero du if

--- src/model/langage/If.java
+++ src/model/langage/If.java
@@ -7,12 +7,10 @@ import src.model.world.*;
  *
  */
 public class If extends ControlFlowStatement {
-	
-	private boolean activated;
+
 
     public If(Personage personage) {
     	super(personage);
-    	activated = false;
         this.addAction(new FinIf(personage));
     }
 
@@ -31,32 +29,34 @@ public class If extends ControlFlowStatement {
         }
 
         public int run(){
-            if(condition.isTrue() && !activated) {
-            	activated = true;
-                return  InstructionEnum.noCostAction.getIdentity();
-            }else {
-            	activated = false;
-            	return -1;
-            }						//condition is not verify
+            if(condition.isTrue())
+                return 0;
+            return -1;						//condition is not verify
         }
     }
-    
-    public int run() {
+
+	public int run() {
+
+        if(actions.peek() == null)       // end actions list for the if
+            return InstructionEnum.endAction.getReturnValue();
+
 		int verification = actions.peek().run();
-		while(verification == InstructionEnum.noCostAction.getIdentity()) {				//do the no count actions.
-			actions.offer(actions.poll());		//add this action in the end of the actions list
+
+		while(verification == InstructionEnum.noCostAction.getIdentity()) {
+			actions.poll();
+            if(actions.peek() == null)
+                return InstructionEnum.noCostAction.getReturnValue();
 			verification = actions.peek().run();
 		}
 
-		if(verification == InstructionEnum.ControlFlowStatementAction.getIdentity()){			//if is a controle flow statement, don't depile
+        if(verification == InstructionEnum.ControlFlowStatementAction.getIdentity()){			//if is a controle flow statement, don't depile
 			return InstructionEnum.ControlFlowStatementAction.getReturnValue();
 		}
 
 		if(verification == InstructionEnum.basicAction.getIdentity()){
-			actions.offer(actions.poll());
-			return InstructionEnum.basicAction.getReturnValue();					//when the list isn't over
+			actions.poll();
+			return InstructionEnum.basicAction.getReturnValue();                          // continu to execute the actions list
 		}
-
-		return InstructionEnum.endAction.getReturnValue();						//when the list of actions is over
+        return InstructionEnum.endAction.getReturnValue();
 	}
 }
