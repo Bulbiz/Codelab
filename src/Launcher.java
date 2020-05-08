eb7ea8ba9e222349289bc743fbc8a2489d44f794
Pholasa Rémy <pholasa2@outlook.fr>
2020-05-08 18:51:48 +0200

    Changer police

--- src/Launcher.java
+++ src/Launcher.java
@@ -16,10 +16,20 @@ import java.io.*;
 public class Launcher{
     //Auxiliary methods used for other purpose :
 
+    private static void changePolice (){
+        UIDefaults dd = UIManager.getDefaults();
+        for(Enumeration e = dd.keys(); e.hasMoreElements();){
+            Object obj = e.nextElement();
+            if(obj.toString().endsWith("font"))
+                UIManager.put(obj,new Font("SANS_SERIF", Font.BOLD,15));
+        }
+    }
+
      public static void main(String[] args) throws Exception {
         EventQueue.invokeLater(() -> {
             try{
                 UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                changePolice();
             }catch(Exception e){
                 //Do nothing
             }
